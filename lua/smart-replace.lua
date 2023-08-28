ad361ff6867d423b66c7f54d9a7df14c62052f1b
Tanmay Vijayvargiya <12tanmayvijay@gmail.com>
2023-08-29 01:21:32 +0530

    Prototype

--- lua/smart-replace.lua
+++ lua/smart-replace.lua
@@ -1,6 +1,6 @@
 local M = {}
-local MIXED_CASE = 1
-local SNAKE_CASE = 2
+local MIXED_CASE = "MIXED_CASE"
+local SNAKE_CASE = "SNAKE_CASE" 
 
 local strsplit = function(inputstr, sep)
         if sep == nil then
@@ -14,21 +14,34 @@ local strsplit = function(inputstr, sep)
 end
 
 local action = {
-  MIXED_CASE = function() end,
-  SNAKE_CASE = function() end,
+  MIXED_CASE = function(words)
+    local str = table.concat(words, "=")
+    return str:gsub("(%l)(%w*)", function(a, b) return a:upper()..b end):gsub("=", "")
+  end,
+  SNAKE_CASE = function(words)
+    return table.concat(words, "_")
+  end,
 }
 
 local FindCase = function(str)
-  return MIXED_CASE
+  local snake_case = str:match("%l+[_%l*]*")
+  if snake_case and #snake_case == #str then return SNAKE_CASE end
+  local mixed_case = str:match("[%u%l*]+")
+  if mixed_case and #mixed_case == #str then return MIXED_CASE end
+  return nil
 end
 
-GetCurrentMatch = function()
-  local match_start = vim.api.nvim_call_function("searchpos", {"", "n"})
-  local match_end = vim.api.nvim_call_function("searchpos", {"", "neW"})
-  local line = vim.api.nvim_call_function("getline", {match_start[1]})
-  print(vim.inspect(match_start))
-  print(vim.inspect(match_end))
-  return vim.api.nvim_call_function("strpart", {line, match_start[2] - 1, match_end[2] - match_start[2] + 1})
+GetCurrentMatch = function(to_list)
+  local match_start = vim.fn.searchpos("", "")
+  local match_end = vim.fn.searchpos("", "ce")
+  local line = vim.fn.getline(match_start[1])
+  local match = vim.fn.strpart(line, match_start[2] - 1, match_end[2] - match_start[2] + 1)
+  local case = FindCase(match)
+  if action[case] then
+    local replacement = action[case](to_list)
+    line = line:gsub(match, replacement, 1)
+    vim.fn.setline(match_start[1], line)
+  end
 end
 
 local SearchPattern = function(pattern)
@@ -36,7 +49,7 @@ local SearchPattern = function(pattern)
 end
 
 local NextMatch = function()
-  local res = vim.api.nvim_call_function("searchcount", {})
+  local res = vim.fn.searchcount()
   return res.total > 0
 end
 
@@ -44,17 +57,30 @@ M.setup = function(opts)
 
 end
 
-replace = function(from, to, del, supported_cases) 
-  del = del or ","
-  supported_cases = supported_cases or ",_"
-  local from_list = strsplit(from, del or ",")
-  local to_list = strsplit(from, del or ",")
+-- TanmayVijayvargiya tanmay_vijayvargiya
+-- TanmayVijayvargiya tanmay_vijayvargiya
+-- TanmayVijayvargiya tanmay_vijayvargiya
 
-  local limit = 100
-  while NextMatch() and limit > 0 do
+local R = function(range, from, to, del, supported_cases) 
+  del = del or " "
+  supported_cases = supported_cases or ",_"
+  local from_list = strsplit(from, del)
+  local to_list = strsplit(to, del)
+  local search_pattern = table.concat(from_list, "{,_}")
+  vim.cmd(range.."Abolish -search "..search_pattern)
+  local limit = vim.fn.searchcount().total 
+  while limit > 0 do
+    local match = GetCurrentMatch(to_list)
     limit = limit - 1
   end
-  
+end
+
+M.ReplaceVisual = function(from, to)
+  R("'<'>", from, to)
+end
+
+M.ReplaceFile = function(from, to)
+  R("'<'>", from, to)
 end
 
 return M
